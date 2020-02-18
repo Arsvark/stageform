50bd4f2b8302d959dfc7084a694727c811016f19
Arsvark <j.laurent928@laposte.net>
2020-02-18 10:21:08 +0100

    -all

---
+++ connet.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="main.css"/>
+    <title>Document</title>
+</head>
+<body>
+    <h1><a href="stagefact1.php">Bienvenue sur le site de facturation</a></h1>
+<?php include ('menu.php'); ?>
+    
+</body>
+</html>
